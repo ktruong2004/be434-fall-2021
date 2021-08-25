76ac18cf64964f3edf35611244cd729ee6e8c614
Khoa <truongkhoa2004@email.arizona.edu>
2021-08-25 09:40:12 -0700

    hello-1

--- examples/01_hello/hello01.py
+++ examples/01_hello/hello01.py
@@ -1,1 +1,19 @@
-print('Hello, World!')
+#!/usr/bin/env python3
+# Say hello
+
+import argparse
+
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', default='World', help='Name to greet')
+    return parser.parse_args()
+
+def main(): 
+    args = get_args()
+    name = args.name
+    print('Hello, ' + name + '!')
+
+if __name__ == '__main__':
+    main()
