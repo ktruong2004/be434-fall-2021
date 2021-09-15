315148f89a602a88544f9db662b2526f95e850f9
Khoa <truongkhoa2004@email.arizona.edu>
2021-09-15 09:47:27 -0700

    final version

--- assignments/02_sum/sum2.py
+++ assignments/02_sum/sum2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author : ktruong <ktruong@localhost>
-Date   : 2021-09-15
+Date   : 2021-09-13
 Purpose: Rock the Casbah
 """
 
@@ -13,7 +13,7 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Picnic game',
+        description='Sum Numbers',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('item',
@@ -37,22 +37,20 @@ def main():
     args = get_args()
     items = args.item
     num = len(items)
-    strings = [str(n) for n in items]
 
     if args.sorted:
         items.sort()
-
-    bringing = ''
     if num == 1:
-        bringing = strings[0]
-        print(''+strings[0] + ' = ' +strings[0])
+        print(f'{items[0]} = {items[0]}')
     elif num == 2:
-        bringing = ' + '.join(strings)
-        print(' + '.join(strings) + ' = ' + str(sum(items)))
+        sum1 = items[0] + items[1]
+        print(f'{items[0]} + {items[1]} = {sum1}')
+    elif num == 3:
+        sum2 = items[0] + items[1] + items[2]
+        print(f'{items[0]} + {items[1]} + {items[2]} = {sum2}')
     else:
-        items[-1] = + items[-1]
-        bringing = ' + '.join(strings)
-        print('{}'.format(bringing) + ' = ' + str(sum(items)))
+        sum3 = items[0] + items[1] + items[2] + items[3]
+        print(f'{items[0]} + {items[1]} + {items[2]} + {items[3]} = {sum3}')
 
 
 # --------------------------------------------------
