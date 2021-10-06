b5e7db3856ca522f56e5c1d24aac79d20bf6d8b5
Khoa <truongkhoa2004@email.arizona.edu>
2021-10-06 09:17:31 -0700

    submit again

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -50,12 +50,12 @@ def main():
 
     k = 3
     seq = args.sequence.upper()
+    for codon in [seq[i:i + k] for i in range(0,len(seq),k)]:
+        print(codon_table.get(codon,'-'),end='',file=args.outfile)
 
-    for codon in [seq[i:i + k] for i in range(0, len(seq), k)]:
-        print(codon_table.get(codon,'-'), end='', file=args.outfile)
-    
     print(file=args.outfile)
-    print(f'Output written to "{args.outfile.name}".') 
+    print(f'Output written to "{args.outfile.name}".')
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
