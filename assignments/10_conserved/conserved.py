c69a07760f1251f6f6ad58af48263df9bf25848d
Khoa <truongkhoa2004@email.arizona.edu>
2021-11-09 20:00:55 -0700

    final submit

---
+++ assignments/10_conserved/conserved.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Author : ktruong <ktruong@localhost>
+Date   : 2021-11-09
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        help='Input file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        default=None)
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    seqs = args.file.read().split()
+    print("\n".join(seqs))
+    for i in range(len(seqs[0])):
+        bases = []
+        for seq in seqs:
+            bases += seq[i]
+        resA = all(x == bases[0] for x in bases)
+        if resA:
+            print("|", end='')
+        else:
+            print("X", end='')
+    print()
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
