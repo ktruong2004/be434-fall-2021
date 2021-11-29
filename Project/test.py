60f6c794534b00ef234d582e3e502ce757750ab2
Khoa <truongkhoa2004@email.arizona.edu>
2021-11-28 17:35:18 -0700

    update test file

---
+++ Project/test.py
@@ -0,0 +1,21 @@
+""" Tests for sudoku.py """
+
+import os
+from subprocess import getstatusoutput
+import re
+
+PRG = './sudoku.py'
+
+
+# --------------------------------------------------
+def test_exists():
+    """ Program exists """
+
+    assert os.path.isfile(PRG)
+
+
+# --------------------------------------------------
+def test_input_1():
+    """ Test Input 1"""
+
+    assert
